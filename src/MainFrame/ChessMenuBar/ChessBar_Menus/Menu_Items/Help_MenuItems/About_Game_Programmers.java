aeb8af7052bd3e74fe97dda69eff1d264a05cccc
saraachten <sara.achten@alumni.msoe.edu>
2017-08-07 22:28:33 -0500

    adds about us window

--- src/MainFrame/ChessMenuBar/ChessBar_Menus/Menu_Items/Help_MenuItems/About_Game_Programmers.java
+++ src/MainFrame/ChessMenuBar/ChessBar_Menus/Menu_Items/Help_MenuItems/About_Game_Programmers.java
@@ -12,6 +12,22 @@ public class About_Game_Programmers extends JMenuItem
     public About_Game_Programmers()
     {
         setText("About us");
+        addActionListener(new ActionListener()
+        {
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		createAboutUsDisplay();
+            
+        	}
+        });
+    }
+    
+    //creates information pop up
+    public void createAboutUsDisplay()
+    {
+    	Frame frame = new JFrame();
+    	JOptionPane.showMessageDialog(frame, "Developers are: \nSara Achten\nDavid Arnold\nRobert Buss\n\nCreated: 2017", 
+    			"About us", JOptionPane.PLAIN_MESSAGE);
     }
     
 }
